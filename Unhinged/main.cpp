b6846fa40942569a389a2871eb693a293988822c
Kaelan van Cleeff <kvancleeff25@g.ucla.edu>
2022-08-27 15:20:43 -0400

    Initial Commit

---
+++ Unhinged/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Unhinged
+//
+//  Created by Kaelan Van Cleeff on 8/27/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
