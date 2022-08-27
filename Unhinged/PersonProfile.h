55bf1dc73b91edacb382b3b08d719a9a3b5244d2
Kaelan van Cleeff <kvancleeff25@g.ucla.edu>
2022-08-27 15:30:36 -0400

    finished

---
+++ Unhinged/PersonProfile.h
@@ -0,0 +1,34 @@
+//
+//  PersonProfile.hpp
+//  Unhinged
+//
+//  Created by Kaelan Van Cleeff on 8/27/22.
+//
+
+#ifndef Personprof
+#define Personprof
+
+#include "provided.h"
+#include "RadixTree.h"
+#include <string>
+#include <vector>
+#include <iostream>
+#include <unordered_set>
+
+class PersonProfile {
+public:
+    PersonProfile(std::string name, std::string email);
+    ~PersonProfile();
+    std::string GetName() const;
+    std::string GetEmail() const;
+    void AddAttValPair(const AttValPair& attval);
+    int GetNumAttValPairs() const;
+    bool GetAttVal(int attribute_num, AttValPair& attval) const;
+private:
+    std::string m_name;
+    std::string m_email;
+    RadixTree<std::unordered_set<std::string>> rt;
+    std::vector<AttValPair> pp;
+};
+
+#endif
