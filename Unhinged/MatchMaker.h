19c3ff11bd4e46105331bfb4b3e3ed580be35455
Kaelan van Cleeff <kvancleeff25@g.ucla.edu>
2022-08-27 15:30:16 -0400

    finished

---
+++ Unhinged/MatchMaker.h
@@ -0,0 +1,26 @@
+//
+//  MatchMaker.hpp
+//  Unhinged
+//
+//  Created by Kaelan Van Cleeff on 8/27/22.
+//
+
+#ifndef MatchMaker_
+#define MatchMaker_
+#include <vector>
+#include <iostream>
+#include "MemberDatabase.h"
+#include "AttributeTranslator.h"
+#include "provided.h"
+
+class MatchMaker {
+public:
+    MatchMaker(const MemberDatabase& mdb, const AttributeTranslator& at);
+    ~MatchMaker();
+    std::vector<EmailCount> IdentifyRankedMatches(std::string email, int threshold) const;
+private:
+    const MemberDatabase* mdb_mm;
+    const AttributeTranslator* at_mm;
+
+};
+#endif
